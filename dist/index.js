118636275b15c5186be27568ae31ab6a59a936b8
daisuke.abe <commityneo@gmail.com>
2024-03-01 10:25:14 +0900

    3.7.2：正規表現

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
-const message = "Hello, world";
-console.log(message);
+// 3.7.2：正規表現
+const r = /ab+c/;
+console.log(r.test("abbbbbc"));
+console.log(r.test("Hello, abc world!"));
+console.log(r.test("ABC"));
+console.log(r.test("こんにちは！"));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const message: string = "Hello, world";
-console.log(message);
+// 3.7.2：正規表現
+const r = /ab+c/;
+
+console.log(r.test("abbbbbc"));
+console.log(r.test("Hello, abc world!"));
+console.log(r.test("ABC"));
+console.log(r.test("こんにちは！"));
