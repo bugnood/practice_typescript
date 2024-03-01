9b3a18d4714e376c9ba22fa23c1926a59f58e30c
daisuke.abe <commityneo@gmail.com>
2024-03-01 14:19:07 +0900

    力試し（自力で進めた部分）

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,30 @@
 "use strict";
-// 3.7.4：Mspオブジェクト、Setオブジェクト
-const map = new Map();
-map.set("foo", 1234);
-console.log(map.get("foo"));
-console.log(map.get("bar"));
+const data = `
+uhyo,26,1
+jhon Smith,17,0
+Mary Sue,14,1
+`;
+// dataの値を格納する配列を作る
+const arr = data.split("\n");
+// User型のオブジェクトを作る
+let userObj = [];
+for (const u of arr) {
+    if (u !== '') {
+        userObj = u.split(",");
+        console.log(userObj);
+    }
+}
+// const userObj: User = "配列を入れる（コンマで区切った値）":
+// オブジェクトの入った配列を作る user(obj1, obj2, obj3...)
+const users = [];
+// for (const user of users) {
+//     if (user.premiumUser) {
+//         console.log(`${user.name}(${user.age})はプレミアムユーザです。`);
+//     } else {
+//         console.log(`${user.name}(${user.age})はプレミアムユーザではありません。`);
+//     }
+// }
+// usersオブジェクトを作る
+// usersの中身は、name,age,premiumUserの3つのプロパティ
+// オブジェクトの入った配列を作る user(obj1, obj2, obj3...)
+// User型のオブジェクトを作る
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
-// 3.7.4：Mspオブジェクト、Setオブジェクト
-const map: Map<string, number> = new Map();
-map.set("foo", 1234);
-console.log(map.get("foo"));
-console.log(map.get("bar"));
+// 3.8.1：力試し
+type User = {
+    name: string,
+    age: number,
+    premiumUser: boolean,
+}
+
+const data: string = `
+uhyo,26,1
+jhon Smith,17,0
+Mary Sue,14,1
+`;
+
+// dataの値を格納する配列を作る
+const arr = data.split("\n");
+
+// User型のオブジェクトを作る
+let userObj = [];
+for (const u of arr) {
+    if (u !== '') {
+        userObj = u.split(",");
+        console.log(userObj);
+    }
+}
+// const userObj: User = "配列を入れる（コンマで区切った値）":
+
+// オブジェクトの入った配列を作る user(obj1, obj2, obj3...)
+const users = [];
+
+for (const user of users) {
+    if (user.premiumUser) {
+        console.log(`${user.name}(${user.age})はプレミアムユーザです。`);
+    } else {
+        console.log(`${user.name}(${user.age})はプレミアムユーザではありません。`);
+    }
+}
+
+// usersオブジェクトを作る
+// usersの中身は、name,age,premiumUserの3つのプロパティ
+// オブジェクトの入った配列を作る user(obj1, obj2, obj3...)
+// User型のオブジェクトを作る
