7329280d777fc9aa5148136ef49b322ddf06f07f
daisuke.abe <commityneo@gmail.com>
2024-03-01 16:35:18 +0900

    メソッド記法

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,10 @@
 "use strict";
-// const calcBMI = ({
-//     height, weight
-// }: Human): number => {
-//     return weight / height ** 2;
-// };
-const uhyo = { height: 1.84, weight: 72 };
-// console.log(calcBMI(uhyo));
-// 4.1.5：アロー関数の省略形
-const calcBMI = ({ height, weight }) => weight / height ** 2;
-console.log(calcBMI(uhyo));
+// 4.1.6：メソッド記法
+const obj = {
+    double(num) {
+        return num * 2;
+    },
+    double2: (num) => num * 2,
+};
+console.log(obj.double(100));
+console.log(obj.double2(-50));
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
-// 4.1.4：アロー関数
-type Human = {
-    height: number,
-    weight: number,
+// 4.1.6：メソッド記法
+const obj = {
+    double(num: number): number {
+        return num * 2;
+    },
+    double2: (num: number): number => num * 2,
 };
-// const calcBMI = ({
-//     height, weight
-// }: Human): number => {
-//     return weight / height ** 2;
-// };
-const uhyo: Human = { height: 1.84, weight: 72 };
-// console.log(calcBMI(uhyo));
-
-// 4.1.5：アロー関数の省略形
-const calcBMI = ({ height, weight }: Human): number => weight / height ** 2;
-console.log(calcBMI(uhyo));
+console.log(obj.double(100));
+console.log(obj.double2(-50));
