26817101b7b2250a6e2a0522814d6fc9070954c8
daisuke.abe <commityneo@gmail.com>
2024-03-01 10:36:35 +0900

    先頭検索

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
 // 3.7.2：正規表現
-const r = /ab+c/;
-console.log(r.test("abbbbbc"));
-console.log(r.test("Hello, abc world!"));
-console.log(r.test("ABC"));
-console.log(r.test("こんにちは！"));
+// const r = /ab+c/;
+const r = /^abc/;
+// console.log(r.test("abbbbbc"));
+// console.log(r.test("Hello, abc world!"));
+// console.log(r.test("ABC"));
+// console.log(r.test("こんにちは！"));
+console.log(r.test("abcdefg"));
+console.log(r.test("Hello, abcdefg"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 // 3.7.2：正規表現
-const r = /ab+c/;
+// const r = /ab+c/;
+const r = /^abc/;
 
-console.log(r.test("abbbbbc"));
-console.log(r.test("Hello, abc world!"));
-console.log(r.test("ABC"));
-console.log(r.test("こんにちは！"));
+// console.log(r.test("abbbbbc"));
+// console.log(r.test("Hello, abc world!"));
+// console.log(r.test("ABC"));
+// console.log(r.test("こんにちは！"));
+
+console.log(r.test("abcdefg"));
+console.log(r.test("Hello, abcdefg"));
