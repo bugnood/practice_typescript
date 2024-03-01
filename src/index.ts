7030d1d8873839a89a02fcb035d7af5df6bd9132
daisuke.abe <commityneo@gmail.com>
2024-03-01 11:21:18 +0900

    Mapオブジェクト、Setオブジェクト

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-// 3.7.3：正規表現を使う方法
-const result = "Hello, abbbbbbc world! abc".match(/a(b+)c/);
-if (result !== null) {
-    console.log(result[0]);
-    console.log(result[1]);
-}
-
-const result_1 = "Hello, abbbbbbc world! abc".match(/a(?<worldName>b+)c/);
-if (result_1 !== null) {
-    console.log(result_1.groups)
-}
+// 3.7.4：Mspオブジェクト、Setオブジェクト
+const map: Map<string, number> = new Map();
+map.set("foo", 1234);
+console.log(map.get("foo"));
+console.log(map.get("bar"));
