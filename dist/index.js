224310a1c68417852d1780768f1c69c1d91eefc1
daisuke.abe <commityneo@gmail.com>
2024-03-01 15:39:29 +0900

    戻り値のない関数

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,19 @@
 "use strict";
-// 4.1.1：関数宣言
-function range(min, max) {
-    const result = [];
-    for (let i = min; i <= max; i++) {
-        result.push(i);
+// 4.1.2：戻り値のない関数宣言
+function helloWorldTimes(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("Hello, world!");
     }
-    return result;
 }
-console.log(range(5, 10));
+console.log(helloWorldTimes(5));
+function helloWorldTimes2(n) {
+    if (n >= 100) {
+        console.log(`${n}回なんて無理です！`);
+        return;
+    }
+    for (let i = 0; i < n; i++) {
+        console.log("Hello, world!");
+    }
+}
+helloWorldTimes2(5);
+helloWorldTimes2(150);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-// 4.1.1：関数宣言
-function range(min: number, max: number): number[] {
-    const result = [];
-    for (let i = min; i <= max; i++) {
-        result.push(i);
+// 4.1.2：戻り値のない関数宣言
+function helloWorldTimes(n: number): void {
+    for (let i = 0; i < n; i++) {
+        console.log("Hello, world!");
     }
-    return result;
 }
-console.log(range(5, 10));
+
+console.log(helloWorldTimes(5));
+
+function helloWorldTimes2(n: number): void {
+    if (n >= 100) {
+        console.log(`${n}回なんて無理です！`);
+        return;
+    }
+    for (let i = 0; i < n; i++) {
+        console.log("Hello, world!");
+    }
+}
+
+helloWorldTimes2(5);
+helloWorldTimes2(150);
