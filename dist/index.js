089f5fa9f80b610bbd6c9c85265fe67d8cef4ab5
daisuke.abe <commityneo@gmail.com>
2024-03-01 10:53:17 +0900

    正規表現を使う方法

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 "use strict";
 // 3.7.3：正規表現を使う方法
-console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/, "foobar"));
-console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/g, "foobar"));
+const result = "Hello, abbbbbbc world! abc".match(/a(b+)c/);
+if (result !== null) {
+    console.log(result[0]);
+    console.log(result[1]);
+}
+const result_1 = "Hello, abbbbbbc world! abc".match(/a(?<worldName>b+)c/);
+if (result_1 !== null) {
+    console.log(result_1.groups);
+}
