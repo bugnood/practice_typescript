d8b5b76dbe18ad51932f2bbfa5d638344281ecc5
daisuke.abe <commityneo@gmail.com>
2024-03-01 16:19:54 +0900

    アロー関数と省略形

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 "use strict";
-const calcBMI = function (human) {
-    return human.weight / human.height ** 2;
-};
+// const calcBMI = ({
+//     height, weight
+// }: Human): number => {
+//     return weight / height ** 2;
+// };
 const uhyo = { height: 1.84, weight: 72 };
+// console.log(calcBMI(uhyo));
+// 4.1.5：アロー関数の省略形
+const calcBMI = ({ height, weight }) => weight / height ** 2;
 console.log(calcBMI(uhyo));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-// 4.1.3：関数式で関数を作る
+// 4.1.4：アロー関数
 type Human = {
     height: number,
     weight: number,
 };
-const calcBMI = function (human: Human): number {
-    return human.weight / human.height ** 2;
-};
+// const calcBMI = ({
+//     height, weight
+// }: Human): number => {
+//     return weight / height ** 2;
+// };
 const uhyo: Human = { height: 1.84, weight: 72 };
+// console.log(calcBMI(uhyo));
+
+// 4.1.5：アロー関数の省略形
+const calcBMI = ({ height, weight }: Human): number => weight / height ** 2;
 console.log(calcBMI(uhyo));
